74a239b17ad338d4a85f5cb806a683887e418817
Pat Shaughnessy <pat@patshaughnessy.net>
2018-11-19 08:52:04 -0800

    Accept output path argument

--- src/bin.rs
+++ src/bin.rs
@@ -4,13 +4,16 @@ use std::env;
 use blog::compile;
 
 pub fn main() {
-    if let Some(input_arg) = env::args().nth(1) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: blogc INPUT_PATH OUTPUT_PATH");
+    } else {
+        let input = &args[1];
+        let output = &args[2];
         let dir = env::current_dir().unwrap();
-        let input_path = dir.join(input_arg);
-        let output_path = dir.join("output_file.txt");
+        let input_path = dir.join(input);
+        let output_path = dir.join(output);
         compile(&input_path, &output_path).expect(&format!("Unable to compile: {:?}\n", input_path));
         println!("Success.");
-    } else {
-        println!("Usage: blogc INPUT_PATH");
     }
 }
