13667d32b229996dbbffe8f73e6ea546055396a9
Pat Shaughnessy <pat@patshaughnessy.net>
2018-11-24 09:05:40 -0800

    Basic layout coded in Maud macros

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,10 +8,28 @@ use blog::compile;
 
 #[test]
 fn it_copies_input_to_output() {
+
     compile(&input_path(), &output_path()).unwrap();
-    let copied = fs::read_to_string(&output_path()).expect("Failed: Didn't find output file.");
-    assert_eq!(copied, "This is an input file.\n");
+
+    let expected_html = fs::read_to_string(&expected_html_path()).expect("Failed: Didn't find expect html file.");
+    let mut expected_html_lines = expected_html.split("\n");
+
+    let actual_html = fs::read_to_string(&output_path()).expect("Failed: Didn't find output file.");
+    let actual_html_separated = str::replace(&actual_html, ">", ">\n");
+    let mut actual_html_lines = actual_html_separated.split("\n");
+    for line in expected_html_lines.zip(actual_html_lines) {
+        //println!();
+        //println!("EXPECTED:");
+        //println!("-------");
+        //println!("{}", line.0.trim());
+        //println!();
+        //println!("ACTUAL:");
+        //println!("-------");
+        //println!("{}", line.1.trim());
+        assert_eq!(line.0.trim(), line.1.trim());
+    }
 }
+
 
 #[test]
 fn it_returns_error_for_invalid_input_path() {
@@ -22,6 +40,10 @@ fn it_returns_error_for_invalid_input_path() {
 
 fn input_path() -> PathBuf {
     tests_path().join("input_file.txt")
+}
+
+fn expected_html_path() -> PathBuf {
+    tests_path().join("summer-school-with-the-rust-compiler-adjusted.html")
 }
 
 fn output_path() -> PathBuf {
