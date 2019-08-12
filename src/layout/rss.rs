cc0f598600e7091dba81893e020db0c7de8c4dc7
Pat Shaughnessy <pat_shaughnessy@apple.com>
2019-08-12 11:43:27 -0700

    done with rss

--- src/layout/rss.rs
+++ src/layout/rss.rs
@@ -12,47 +12,48 @@ use post::Post;
 pub fn render(all_posts: &Vec<Post>) -> String {
     let system_time = SystemTime::now();
     let now: DateTime<Utc> = system_time.into();
+    let now = now.to_rfc3339_opts(SecondsFormat::Secs, true);
     let rendered = html! {
       feed xmlns="http://www.w3.org/2005/Atom" {
         title { "Pat Shaughnessy" }
         id { "http://patshaughnessy.net" }
         updated { 
-          (now.to_rfc3339_opts(SecondsFormat::Secs, true))
+          (now)
         }
         author {
           name { "Pat Shaughnessy" }
         }
         @for post in all_posts.iter().take(10) {
+          @let absolute_url = format!("http://patshaughnessy.net/{}", post.url);
+          @let date = post.date.to_rfc3339_opts(SecondsFormat::Secs, true);
           entry {
             title {
               (post.title)
             }
-            // Please save this in a local variable
-              link href={"http://patshaughnessy.net/" (post.url)} rel="alternate" { }
-              id href={"http://patshaughnessy.net/" (post.url)} rel="alternate" { }
-              published { 
-                (post.date.to_rfc3339_opts(SecondsFormat::Secs, true))
+            link href={ (absolute_url) } rel="alternate" { }
+            id href={ (absolute_url) } rel="alternate" { }
+            published { 
+              (date)
+            }
+            updated { 
+              (date)
+            }
+            category {
+              @if let Some(ref t) = post.tag {
+                  (t)
+              } @else {
+                  ""
               }
-              updated { 
-                (post.date.to_rfc3339_opts(SecondsFormat::Secs, true))
-              }
-              category {
-                @if let Some(ref t) = post.tag {
-                    (t)
-                } @else {
-                    ""
-                }
-              }
-              author {
-                name { "Pat Shaughnessy" }
-              }
-              summary {
-                  // TODO: Has to be escaped HTML
-                  (post.summary())
-              }
-              content {
-                  (post.content)
-              }
+            }
+            author {
+              name { "Pat Shaughnessy" }
+            }
+            summary type="html" {
+                (post.summary())
+            }
+            content type="html" {
+                (post.content)
+            }
           }
         }
       }
